df64afb32601b8effb3389f4458beaf974041b11
Kieran Dominy <38659105+MrKren@users.noreply.github.com>
2023-03-08 00:43:43 +0000

    Add html to bundling

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,4 +1,5 @@
-const path = require('path')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: path.resolve(__dirname, 'src', 'index.tsx'),
@@ -15,8 +16,14 @@ module.exports = {
     resolve: {
         extensions: ['.tsx', '.ts']
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "src", "index.html")
+        })
+    ],
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
     }
-}
+};
